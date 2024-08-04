ff65b180531d5d496e2a06fc2aa1dd8bf26536a2
Raj Kamble <rajskamble21@gmail.com>
2024-08-04 18:35:26 +0530

    accordian added

--- js/main.js
+++ js/main.js
@@ -17,3 +17,31 @@ document.addEventListener('DOMContentLoaded', function () {
         }
     });
 });
+
+
+
+
+
+
+document.querySelectorAll('.accordion-header').forEach(header => {
+    header.addEventListener('click', () => {
+        const content = header.nextElementSibling;
+        const icon = header.querySelector('i');
+
+        if (content.style.display === 'block') {
+            content.style.display = 'none';
+            icon.classList.replace('fa-minus', 'fa-plus');
+        } else {
+            document.querySelectorAll('.accordion-content').forEach(item => {
+                item.style.display = 'none';
+            });
+            document.querySelectorAll('.accordion-header i').forEach(item => {
+                item.classList.replace('fa-minus', 'fa-plus');
+            });
+
+            content.style.display = 'block';
+            icon.classList.replace('fa-plus', 'fa-minus');
+        }
+    });
+});
+
